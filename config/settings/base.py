95834a46e16bb1f517e2b7c26bfb83f0be21ff06
Roman S <80767090+Ridmel@users.noreply.github.com>
2022-12-24 20:18:59 +0300

    Feature/initialize porject (#3)
    subj

---
+++ config/settings/base.py
@@ -0,0 +1,135 @@
+from pathlib import Path
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#secret-key
+SECRET_KEY = env.str(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-#4!pz=idpegh$47_#ry6^@6xf!osm0^=_c63u2rf+5k)nsd16i",
+)
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#debug
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", []) + ["localhost", "127.0.0.1"]
+
+
+DJANGO_APPS = [
+    "django.contrib.sites",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+LOCAL_APPS = [
+    "apps.users",
+]
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#installed-apps
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#root-urlconf
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+SITE_ID = 1
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+LANGUAGE_CODE = "ru-RU"
+TIME_ZONE = "Europe/Moscow"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = "/static/"
+# https://docs.djangoproject.com/en/3.2/ref/settings/#static-root
+STATIC_ROOT = BASE_DIR / "apps" / "staticfiles"
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#media-url
+MEDIA_URL = "/media/"
+# https://docs.djangoproject.com/en/3.2/ref/settings/#media-root
+MEDIA_ROOT = BASE_DIR / "apps" / "media"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-user-model
+AUTH_USER_MODEL = "users.User"
+
+# Django REST framework
+REST_FRAMEWORK = {}
