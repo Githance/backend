c180c8bc9bddf6be81b841ba2127d4d1caab41a6
macbook pro <firsov.ip@gmail.com>
2022-12-31 00:29:22 +0300

    docker settings added

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# TODO: use slim version later
+FROM python:3.10.5
+WORKDIR /app
+COPY requirements.txt .
+RUN python3 -m pip install --upgrade pip
+RUN pip3 install -r requirements.txt
+COPY apps/ .
+COPY config/ .
+COPY manage.py .
+CMD gunicorn config.wsgi:application -c /app/config/gunicorn.conf.py
--- config/settings/base.py
+++ config/settings/base.py
@@ -79,11 +79,14 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env.str("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": env.str("DB_NAME", BASE_DIR / "db.sqlite3"),
+        "USER": env.str("POSTGRES_USER"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT")
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
