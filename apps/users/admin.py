80d8ec18f8787c1c248ab0349d63219af37f627b
macbook pro <firsov.ip@gmail.com>
2023-01-31 23:30:36 +0300

    dev workflow/deploy: setting nginx v.0.18

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
