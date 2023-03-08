c8fd0ad7e55e5e8e1e75aa8dfd4dcbf868e61eb8
macbook pro <firsov.ip@gmail.com>
2023-03-09 00:44:28 +0300

    created models

---
+++ apps/participants/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from apps.projects.models import AbstractClassifier, Project, User
+
+
+class Role(AbstractClassifier):
+
+    class Meta:
+        verbose_name = "Проектная роль"
+        verbose_name_plural = "Проектные роли"
+
+
+class Profession(AbstractClassifier):
+
+    class Meta:
+        verbose_name = "Профессия"
+        verbose_name_plural = "Профессии"
+
+
+class Participant(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='participants'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.RESTRICT,
+        related_name='projects'
+    )
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.RESTRICT,
+    )
+    profession = models.ForeignKey(
+        Profession,
+        on_delete=models.RESTRICT,
+    )
+    join_date = models.DateTimeField(
+        "Дата присоединения",
+        auto_now_add=True,
+    )
+    leave_date = models.DateTimeField(
+        "Дата прекращения участия",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
---
+++ apps/projects/models.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class AbstractClassifier(models.Model):
+    """Abstract model for standard classifier."""
+
+    name = models.CharField("Название", max_length=100)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectType(AbstractClassifier):
+
+    class Meta:
+        verbose_name = "Тип проекта"
+        verbose_name_plural = "Типы проекта"
+
+
+class ProjectStatus(AbstractClassifier):
+
+    class Meta:
+        verbose_name = "Статус проекта"
+        verbose_name_plural = "Статусы проекта"
+
+
+class Project(AbstractClassifier):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.RESTRICT,
+        verbose_name="Владелец",
+        related_name="own_projects",
+    )
+    types = models.ManyToManyField(
+        ProjectType,
+        verbose_name="Типы проекта",
+    )
+    status = models.ForeignKey(
+        User,
+        on_delete=models.SET_DEFAULT,
+        verbose_name="Статус",
+    )
+    intro = models.CharField(
+        "Краткое описание",
+        max_length=192,
+    )
+    description = models.CharField(
+        "Подробное описание",
+        max_length=2000,
+    )
+    created_date = models.DateTimeField(
+        "Дата создания",
+        auto_now_add=True,
+    )
+    last_top_date = models.DateTimeField(
+        "Дата последнего события",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
