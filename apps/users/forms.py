5ea916f20f0766bd2c9a324308ba4aa852e6aca1
Roman Sergienko <80767090+Ridmel@users.noreply.github.com>
2023-03-12 04:53:11 +0300

    fix import order

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import (
-    UserChangeForm as DjangoUserChangeForm,
-    UserCreationForm as DjangoUserCreationForm,
-)
+from django.contrib.auth.forms import UserChangeForm as DjangoUserChangeForm
+from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 
 User = get_user_model()
 
