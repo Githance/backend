58a42ee2b7e9f09cbc7f2a2f255b7e22bb86d202
Roman Sergienko <80767090+Ridmel@users.noreply.github.com>
2023-03-28 05:07:13 +0300

    delete user name from email template, add max_length to email field, disable punycode converion for email

--- apps/users/validators.py
+++ apps/users/validators.py
@@ -1,6 +1,7 @@
 import re
 
 from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator as DjangoEmailValidator
 
 
 def validate_telegram_name(value):
@@ -10,3 +11,20 @@ def validate_telegram_name(value):
             "Имя в телеграме может состоять только из английских букв, цифр и _. "
             "Длина от 5 до 32 символов."
         )
+
+
+class EmailValidator(DjangoEmailValidator):
+    # Like the original __call__ , but it doesn't try to convert non-ASCII to punycode.
+    def __call__(self, value):
+        if not value or "@" not in value:
+            raise ValidationError(self.message, code=self.code, params={"value": value})
+
+        user_part, domain_part = value.rsplit("@", 1)
+
+        if not self.user_regex.match(user_part):
+            raise ValidationError(self.message, code=self.code, params={"value": value})
+
+        if domain_part not in self.domain_allowlist and not self.validate_domain_part(
+            domain_part
+        ):
+            raise ValidationError(self.message, code=self.code, params={"value": value})
