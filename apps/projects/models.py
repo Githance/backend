8a9ef3925944987bf225e7a34f67223f187f3867
Roman Sergienko <80767090+Ridmel@users.noreply.github.com>
2023-03-31 16:41:49 +0300

    change users migrations, add telegram and email fields to Project model

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils import timezone
 
 from apps.core.models import BaseModel
+from apps.core.validators import EmailValidator, validate_telegram_name
 
 User = get_user_model()
 
@@ -13,7 +15,6 @@ class Project(BaseModel):
         VACANCY = ("vacancy", "идет набор")
         IN_PROGRESS = ("in_progress", "в процессе")
         CLOSED = ("closed", "закрыт")
-
     name = models.CharField(
         "Название",
         max_length=32,
@@ -39,6 +40,21 @@ class Project(BaseModel):
         max_length=2000,
         null=True,
         blank=True,
+    )
+    # telegram username is stored without '@'
+    telegram = models.CharField(
+        verbose_name="Телеграм",
+        null=True,
+        blank=True,
+        max_length=32,
+        validators=(MinLengthValidator(5), validate_telegram_name),
+    )
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=254,
+        null=True,
+        blank=True,
+        validators=(EmailValidator(),),
     )
     last_top_at = models.DateTimeField(
         "В топе последний раз",
