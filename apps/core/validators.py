ab85cb96b3d9391ce2043af10c5f1c7f808a0476
Roman Sergienko <80767090+Ridmel@users.noreply.github.com>
2023-05-27 14:05:08 +0300

    change validation of domain part of email

--- apps/core/validators.py
+++ apps/core/validators.py
@@ -6,7 +6,10 @@ from django.utils.regex_helper import _lazy_re_compile
 
 
 def validate_telegram_name(value):
-    regex = re.compile(r"^[a-z][a-z0-9_]{3,30}[a-z0-9]$", flags=re.IGNORECASE)
+    regex = re.compile(
+        r"^[a-z][a-z0-9_]{3,30}[a-z0-9]$|^@[a-z][a-z0-9_]{4,31}[a-z0-9]$",
+        flags=re.IGNORECASE
+    )
     if not regex.search(str(value)):
         raise ValidationError(
             "Имя в телеграме может состоять только из английских букв, цифр и _. "
@@ -26,6 +29,7 @@ class EmailValidator(DjangoEmailValidator):
     )
     # Punycode to exclude.
     punycode = _lazy_re_compile(r"^xn--|\.xn--", re.IGNORECASE)
+    forbidden_symbols = _lazy_re_compile(r"[^A-Z0-9-\.]", re.IGNORECASE)
 
     # https://github.com/Githance/testing/issues/15
     # Like the original __call__ , but it doesn't try to convert non-ASCII to punycode,
@@ -40,7 +44,8 @@ class EmailValidator(DjangoEmailValidator):
             raise ValidationError(self.message, code=self.code, params={"value": value})
 
         if domain_part not in self.domain_allowlist and (
-            self.punycode.match(domain_part)
+            self.forbidden_symbols.match(domain_part)
+            or self.punycode.match(domain_part)
             or not self.validate_domain_part(domain_part)
         ):
             raise ValidationError(self.message, code=self.code, params={"value": value})
